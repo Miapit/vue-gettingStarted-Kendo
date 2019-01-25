00b9c58eb430612ce1026f7e230fee33275b87a7
Mihail Parvanov <mihail.parvanov@telerik.com>
2019-01-25 09:58:38 +0200

    Add Kendo buttons with font icons to the template

---
+++ kendoui-vue/src/components/TermList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="term-list">
+    <div class="term-box">
+      <div class="term">
+        <h1> GIF </h1>
+        <p class="term-definition">
+          a lossless format for image files that support both animated and static images.
+        </p>
+        <div class="term-pronunciation">
+          <input type="radio" v-model="selected" name="vote" :value="0" />
+          <kendo-button
+            icon='volume-up'
+            @click='playSound("http://bit.ly/2HHjJio")'
+            > gif
+          </kendo-button>
+          <kendo-button
+            icon='volume-down'
+            @click='playSound("http://bit.ly/2plm002")'
+            > gif
+          </kendo-button>
+        </div>
+        <div class="term-voting">
+          <kendo-button class='k-primary' @click='onVote'>
+            VOTE
+          </kendo-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'term-list',
+  data () {
+    return {
+      selected: 0,
+      pronunciation1Votes: 0,
+      pronunciation2Votes: 0
+    }
+  },
+  methods: {
+    playSound (sound) {
+      // if(sound){
+      //   let audio = new Audio(sound)
+      //   audio.play(play)
+      // }
+    },
+    onVote () {
+      alert()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
--- kendoui-vue/src/main.js
+++ kendoui-vue/src/main.js
@@ -2,12 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import {
+  Buttons,
+  ButtonsInstaller
+} from '@progress/kendo-buttons-vue-wrapper'
+import '@progress/kendo-theme-default/dist/all.css'
+import '@progress/kendo-ui'
 
 Vue.config.productionTip = false
+Vue.use(ButtonsInstaller)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: {
+    App,
+    Buttons
+  }
 })
